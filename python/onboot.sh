1c16df133e07a9fa882a8cd194dae3b4f07d54ad
huangshenyuan-unice <shenyuan.huang@etu.unice.fr>
2020-02-16 00:20:01 +0100

    use systemctl to start daemon

--- python/onboot.sh
+++ python/onboot.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=./
 while true; do
-	mosquitto=`ps aux | grep mosquitto | grep -v grep`
-	main=`ps aux | grep python | grep -v grep`
+	mosquitto=`ps aux | grep "mosquitto -p 1884" | grep -v grep`
+	main=`ps aux | grep ./main.py | grep -v grep`
 	if [ ! "$mosquitto" ]; then
 		mosquitto -p 1884 1>>./log/journalMos 2>&1 &
 	fi
@@ -11,5 +11,21 @@ while true; do
 	fi
 	sleep 10
 done
-#sudo nano /etc/rc.local
-#before exit 0 add su pi -c "exec /home/pi/Desktop/OCS-2019-master/python/onboot.sh" 
+
+#sudo nano /etc/systemd/system/alois.service
+# [Unit]
+# Description=My service
+# After=network.target
+
+# [Service]
+# ExecStart=/bin/bash /home/pi/Desktop/OCS-2019-master/python/onboot.sh
+# WorkingDirectory=/home/pi/Desktop/OCS-2019-master/python/
+# StandardOutput=inherit
+# StandardError=inherit
+# Restart=always
+# User=pi
+
+# [Install]
+# WantedBy=multi-user.target
+# sudo systemctl enable alois.service
+
